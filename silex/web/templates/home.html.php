9d3619e6ede1dece8a942caf7b388edf19589ba7
Tobias Wittwer <t.wittwer95@gmail.com>
2015-02-03 21:15:24 +0100

    main layout template

--- silex/web/templates/home.html.php
+++ silex/web/templates/home.html.php
@@ -6,6 +6,18 @@
  * Time: 20:41
  */
 
-$view->extend('layout.html.php') ?>
+$view->extend('layout.html.php');
+$view['slots']->set('title', "Home");
+?>
 
-<?php $view['slots']->set('title', "Home") ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="jumbotron">
+            <h1>My first Bootstrap website!</h1>
+
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing.</p>
+
+            <p><a class="btn btn-primary btn-lg" role="button" href="http://www.google.com/"><span class="glyphicon glyphicon-search" aria-hidden="true"></span> Search</a></p>
+        </div>
+    </div>
+</div>
--- silex/web/templates/layout.html.php
+++ silex/web/templates/layout.html.php
@@ -1,12 +1,51 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Tobias Wittwer
- * Date: 03.02.2015
- * Time: 20:43
- */
+/** Main Layout Template */
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Webengine<?php $view['slots']->output('title', 'DEF') ?></title>
+    <!-- Bootstrap - Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <!-- Bootstrap - Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+    <!-- JQuery -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <!-- Bootstrap - JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">WebSiteName</a>
+        </div>
 
-<?php $view['slots']->output(' | '.'title', '') ?>
-
-<?php $view['slots']->output('_content') ?>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#"><span class="glyphicon glyphicon-home"
+                                                     aria-hidden="true"></span>Home<span
+                            class="sr-only">(current)</span></a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-music" aria-hidden="true"></span> Music</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Profile</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Settings</a></li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
+<div class="container">
+    <?php $view['slots']->output('_content') ?>
+</div>
+</body>
+</html>
